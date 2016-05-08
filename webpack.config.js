0d86132b5b8822d5824c83bafc6b0d5ee7513462
abramenal <paul.ovramenko@gmail.com>
2016-05-08 15:00:03 +0300

    fixed issue with webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,20 @@ module.exports = {
   entry: './public/javascripts/ng-app.js',
   output: {
     path: path.join(__dirname, './public/dist/'),
-    filename: "bundle.js"
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-      { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
-    ]
-  }
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      }, {
+        test: /\.(ttf|eot|woff|woff2|svg)$/,
+        loader: 'url-loader?limit=50000&name=fonts/[name].[ext]',
+      }, {
+        test: /\.(png|jpe?g)$/,
+        loader: 'url-loader?limit=50000&name=images/[name].[ext]',
+      },
+    ],
+  },
 };
